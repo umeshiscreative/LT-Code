01478316135104f8e325075517f9fe8094ae3369
Umesh Verma <umeshiscreative@gmail.com>
2026-05-12 12:59:31 +0530

    iterative version added

--- app/src/main/java/com/umesh/codepractice/KnapSack.java
+++ app/src/main/java/com/umesh/codepractice/KnapSack.java
@@ -30,35 +30,56 @@ public class KnapSack {
     //     }
     //     return -1;
     // }
+    // Memoized Version passed all the testcases
+    // public int knapsack(int W, int val[], int wt[]) {
+    //     int idx = wt.length;
+    //     dp = new int[wt.length + 1][W + 1];
+    //     for (int row = 0; row < idx + 1; row++) {
+    //         for (int col = 0; col < W + 1; col++) {
+    //             dp[row][col] = -1;
+    //         }
+    //     }
+    //     return helper(W, val, wt, idx);
+    // }
+    // public int helper(int W, int val[], int wt[], int index) {
+    //     if (index == 0 || W == 0) {
+    //         return 0;
+    //     }
+    //     if (dp[index][W] != -1) {
+    //         return dp[index][W];
+    //     }
+    //     if (wt[index - 1] <= W) {
+    //         dp[index][W] = Math.max(
+    //                 val[index - 1] + helper(W - wt[index - 1], val, wt, index - 1),
+    //                 helper(W, val, wt, index - 1));
+    //     } else if (wt[index - 1] > W) {
+    //         dp[index][W] = helper(W, val, wt, index - 1);
+    //     }
+    //     return dp[index][W];
+    // }
+    //Iterative Version
     public int knapsack(int W, int val[], int wt[]) {
-        int idx = wt.length;
-        dp = new int[wt.length + 1][W + 1];
+        int rows = wt.length;
+        int cols = W;
+        int[][] t = new int[rows + 1][cols + 1];
 
-        for (int row = 0; row < idx + 1; row++) {
-            for (int col = 0; col < W + 1; col++) {
-                dp[row][col] = -1;
+        // Initializ the matrix
+        for (int row = 0; row < rows + 1; row++) {
+            for (int col = 0; col < cols + 1; col++) {
+                t[row][col] = 0;
             }
         }
 
-        return helper(W, val, wt, idx);
-    }
-
-    public int helper(int W, int val[], int wt[], int index) {
-        if (index == 0 || W == 0) {
-            return 0;
+        for (int row = 1; row < rows + 1; row++) {
+            for (int col = 1; col < cols + 1; col++) {
+                if (wt[row - 1] <= col) {
+                    t[row][col] = Math.max(val[row - 1] + t[row - 1][col - wt[row - 1]],
+                            t[row - 1][col]);
+                } else if (wt[row - 1] > col) {
+                    t[row][col] = t[row - 1][col];
+                }
+            }
         }
-
-        if (dp[index][W] != -1) {
-            return dp[index][W];
-        }
-
-        if (wt[index - 1] <= W) {
-            dp[index][W] = Math.max(
-                    val[index - 1] + helper(W - wt[index - 1], val, wt, index - 1),
-                    helper(W, val, wt, index - 1));
-        } else if (wt[index - 1] > W) {
-            dp[index][W] = helper(W, val, wt, index - 1);
-        }
-        return dp[index][W];
+        return t[rows][cols];
     }
 }
